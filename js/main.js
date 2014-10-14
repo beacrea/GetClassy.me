c1742dc3e636a189798acdea9360a57b4b333a68
Coty Beasley <coty@cotybeasley.name>
2014-10-14 18:24:50 -0500

    Added some easter eggs and a url param puller

--- js/main.js
+++ js/main.js
@@ -7,9 +7,23 @@ $.preloadImages = function() {
     for (var i = 0; i < arguments.length; i++) {
         $("<img />").attr("src", arguments[i]);
     }
-}
+};
 
 $.preloadImages("../img/app/rainAnim.gif","../img/app/fireAnim.gif");
+
+/*
+ * -------------------------------------------
+ *	$_URL PARAM LOADER
+ * -------------------------------------------
+ */
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 
 /*
  * -------------------------------------------
@@ -32,3 +46,56 @@ $('.wrapper_rain, .wrapper_fire').click(function() {
 
 audioLayerToggle('rain');
 audioLayerToggle('fire');
+
+
+/*
+ * -------------------------------------------
+ *	$_AUDIO LAYER CONTROL
+ * -------------------------------------------
+ */
+
+var ytLoad = function(videoID, listID) {
+    if (typeof listID !== 'undefined' ) {
+        $('#ytPlayer').attr('src', '//www.youtube.com/embed/' + videoID + '?list=' + listID + '&autoplay=1');
+    } else {
+        $('#ytPlayer').attr('src', '//www.youtube.com/embed/' + videoID + '&autoplay=1');
+    }
+
+};
+
+// Default Music
+ytLoad('sCQfTNOC5aE', 'PL-4U2d6ASRHn-h6UdaKkKnaAhg2T2kxAj');
+
+$(function() {
+    var url = getParameterByName('ytID');
+    if (typeof listID !== 'undefined' ) {
+        ytLoad(getParameterByName('ytID'), '');
+    }
+});
+
+/*
+ * -------------------------------------------
+ *	$_EASTEREGGS
+ * -------------------------------------------
+ */
+
+var eastereggs = function() {
+    console.log(
+        'The following commands will play various songs.\n' +
+        '===============================================\n' +
+        'ocean() = Frank Ocean\'s "Thinking About You"\n' +
+        'shoulda() = Jamie Woon\'s "Shoulda"\n' +
+        'sexualheal() = Hot Chip\'s "Sexual Healing"'
+    );
+};
+var ocean = function() {
+    ytLoad('wD8nAt2xJZM', '');
+};
+
+var shoulda = function() {
+    ytLoad('oUm4MHEeFCY', '');
+};
+
+var sexualheal = function() {
+    ytLoad('kYt_TDcIPT0', '');
+};
